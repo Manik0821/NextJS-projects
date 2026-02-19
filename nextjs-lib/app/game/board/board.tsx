1580c0102df701936d5d9cfadca07d8e4063ef9c
Manik Jain <maniknahta@gmail.com>
2026-02-19 23:41:48 +0530

    formatting game

--- nextjs-lib/app/game/board/board.tsx
+++ nextjs-lib/app/game/board/board.tsx
@@ -22,35 +22,35 @@ const Board = () => {
     // const count = useCounterStore((state) => state.count);
     // const increment = useCounterStore((state) => state.increment);
 
-  return (
-    <div className='boardWrapper'>
+    return (
+        <div className='boardWrapper'>
 
-        <h2 className='boardHeading'>Board</h2>
-        <div className="playerBoard">
-            <div className={`player ${currPlayer === 'X' ? 'active' : ''}`}>
-                Player A (X) - {useGameStore((state) => state.ScoreA)}
-            </div>
-            <div className={`player ${currPlayer === 'O' ? 'active' : ''}`}>
-                Player B (O) - {useGameStore((state) => state.ScoreB)}
-            </div>
-        </div>
-        <div className="board">
-            {board.map((value, index) => (
-                <div key={index} onClick={() => useGameStore.getState().updateBoard(index, currPlayer)}>
-                    <Tile value={value} />
+            <h2 className='boardHeading'>Board</h2>
+            <div className="playerBoard">
+                <div className={`player ${currPlayer === 'X' ? 'active' : ''}`}>
+                    Player A (X) - {useGameStore((state) => state.ScoreA)}
                 </div>
-            ))}
-        </div>
-        <div className="playerBoard">
-            <div className={`player active`} onClick={() => useGameStore.getState().resetBoard()}>
-                Reset Board 
+                <div className={`player ${currPlayer === 'O' ? 'active' : ''}`}>
+                    Player B (O) - {useGameStore((state) => state.ScoreB)}
+                </div>
             </div>
-            <div className={`player active`} onClick={() => useGameStore.getState().resetGame()}>
-                Reset Game 
+            <div className="board">
+                {board.map((value, index) => (
+                    <div key={index} onClick={() => useGameStore.getState().updateBoard(index, currPlayer)}>
+                        <Tile value={value} />
+                    </div>
+                ))}
+            </div>
+            <div className="playerBoard">
+                <div className={`player active`} onClick={() => useGameStore.getState().resetBoard()}>
+                    Reset Board
+                </div>
+                <div className={`player active`} onClick={() => useGameStore.getState().resetGame()}>
+                    Reset Game
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Board;
--- nextjs-lib/app/game/tile.tsx
+++ nextjs-lib/app/game/tile.tsx
@@ -8,15 +8,14 @@ type TileProps = {
 // Updated Component with enhanced Tailwind styles
 const Tile: React.FC<TileProps> = ({ value }) => {
     return (
-      <div className="flex justify-center items-center">
-          <div className="tileWrapper transition-transform hover:scale-105 active:scale-95 cursor-pointer">
-              <div className={`tile flex items-center justify-center w-16 h-16 p-4 border-2 border-gray-500 rounded-lg bg-white shadow-md text-2xl font-bold uppercase tile-${value}` }>
-                  {value === '' ? <span className="opacity-0">A</span> : value}
-              </div>
-          </div>
-      </div>
+        <div className="flex justify-center items-center">
+            <div className="tileWrapper transition-transform hover:scale-105 active:scale-95 cursor-pointer">
+                <div className={`tile flex items-center justify-center w-16 h-16 p-4 border-2 border-gray-500 rounded-lg bg-white shadow-md text-2xl font-bold uppercase tile-${value}`}>
+                    {value === '' ? <span className="opacity-0">A</span> : value}
+                </div>
+            </div>
+        </div>
     );
-  }
-  
+}
 
 export default Tile;
